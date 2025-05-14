d008c946a6c4a40cee3940bdc05d9d59358af0e0
Aaro Koinsaari <aaro.koinsaari@protonmail.ch>
2025-05-14 11:48:14 +0200

    fix: reset scroll state when page is changed

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Analytics } from '@vercel/analytics/react';
+import ScrollToTop from './components/ScrollToTop';
 import HomePage from './pages/home/HomePage';
 import MenuPage from './pages/menu/MenuPage';
 import LunchMenuPage from './pages/lunch_menu/LunchMenuPage';
@@ -16,6 +17,7 @@ import PrivacyPolicyPage from './pages/privacy/PrivacyPolicyPage';
 export default function App() {
   return (
     <Router>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/menu" element={<MenuPage />} />
---
+++ src/components/ScrollToTop.jsx
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2025 Aaro Koinsaari
+ * Licensed under the MIT License.
+ */
+
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+
+export default function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
